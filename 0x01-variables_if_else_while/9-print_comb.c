6e30330192e75770561da9e98e27356595637441
Kedir Seid <kedirseid965@gmail.com>
2022-12-20 03:58:18 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combination of single digit number
+ * Return: Always 0
+ */
+int main(void)
+{
+	int la;
+
+	for (la = 48; la < 58; la++)
+	{
+		putchar(la);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
