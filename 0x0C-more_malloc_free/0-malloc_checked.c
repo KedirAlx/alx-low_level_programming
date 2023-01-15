eb4878daf9d47ed55dfd7057a1c76bc83c88e00c
Kedir Seid <kedirseid965@gmail.com>
2023-01-15 12:00:54 -0800

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,6 +3,7 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocates
+ *
  * Return: a pointer to the allocated memory.
  * if malloc fails, status value is equal to 98.
  */
